dde578f9c12e7c42a751c27675e96674b56aeec0
Idris Lokhandwala <idrisfl@gmail.com>
2021-01-12 22:27:33 +0100

    Api gateway using Ocelot for redirecting to 2 separate APIs

---
+++ OrdersApi/Controllers/OrdersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OrdersApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        // GET: api/<OrdersController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Order1", "Order2", "Order3" };
+        }
+
+        // GET api/<OrdersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Order" + id;
+        }
+    }
+}
---
+++ ProductsApi/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "product1", "product2" };
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "product" + id;
+        }
+    }
+}
